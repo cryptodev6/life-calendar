5ee23ec1f4344c5cc101c96f41f7a6bd49023a1a
cryptodev <fullstackagency123@gmail.com>
2024-05-24 22:20:58 -0500

    changed styling

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+const lifeStages = [
+    { stage: "Bebé", startYear: 0, endYear: 1 },
+    { stage: "Niño pequeño", startYear: 1, endYear: 3 },
+    { stage: "Preescolar", startYear: 3, endYear: 6 },
+    { stage: "Edad escolar", startYear: 6, endYear: 12 },
+    { stage: "Adolescencia temprana", startYear: 12, endYear: 14 },
+    { stage: "Adolescencia media", startYear: 14, endYear: 16 },
+    { stage: "Adolescencia tardía", startYear: 16, endYear: 18 },
+    { stage: "Adulto joven", startYear: 18, endYear: 25 },
+    { stage: "Adultez temprana", startYear: 25, endYear: 40 },
+    { stage: "Adultez media", startYear: 40, endYear: 65 },
+    { stage: "Adultez tardía", startYear: 65, endYear: 90 }
+];
+
 function calculateWeeks() {
     const birthdate = new Date(document.getElementById('birthdate').value);
     const today = new Date();
@@ -23,9 +37,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const weeksLabelsContainer = document.querySelector('.weeks-labels');
     for (let i = 1; i <= 52; i++) {
         const span = document.createElement('span');
-        if (i === 1 || i % 5 === 0) {
-            span.textContent = i;
-        }
+        span.textContent = i;
         weeksLabelsContainer.appendChild(span);
     }
 
